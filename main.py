6f688c39b3e57b51d326cf93f5ce2d1cfbbcd8b9
boucheri <boucheri@battelle.org>
2026-04-09 18:42:14 -0400

    minor updates

--- main.py
+++ main.py
@@ -14,16 +14,17 @@ def ReadInputFile(input_filepath) -> InputData:
     # TODO: read in input file with spherical harmonic specifications
     #  worth noting: these specifications will go into the c++ code.
     #  Hopefully we can avoid reading json in c++ by doing this, but could limit what arguments we can set
-    return InputData("test_path.exe", "test_output.csv")
+    return InputData("test_output.csv")
 
 
-def RunCalculator(input_data: InputData):
-    # TODO: use subprocess to call executable at path in input data
-    pass
+def BuildAndRunCalculator(input_data: InputData):
+    sp.run("make -f make_spherical_harmonic_generator") # TODO: not hardcode filename?
+    sp.run("./SphericalHarmonicGenerator.x") # TODO: correct name? also need to pass in runtime arguments from input file
 
 
 def VisualizeOutputs():
     # TODO: given fourier transform data of the specified spherical harmonic, plot the data
+    #  also plot spherical harmonic
     #  (somehow, idk - maybe matplotlib, maybe something fancier like seaborn, TBD)
     pass
 
@@ -32,13 +33,13 @@ def VisualizeOutputs():
 
 def main():
     # TODO: create optional argument for specifying input filepath location
-    input_file_filepath = ""
+    input_file_filepath = "sample_input.json"
 
     input_data = ReadInputFile(input_file_filepath)
 
-    RunCalculator(input_data)
+    BuildAndRunCalculator(input_data)
 
-    calculator_results = pd.read_csv(input_data.calc_output_path) # may not be csv, TBD
+    calculator_results = pd.read_csv(input_data.calc_output_path) # TODO: may not be csv, TBD
     VisualizeOutputs(calculator_results)
 
 
